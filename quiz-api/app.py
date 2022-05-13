cdfe0fb4beba8b74e4bf6fc8d2c75f07df395a6a
Richard FOUQUOIRE <richard.fouquoire@edu.esiee.fr>
2022-05-13 16:18:33 +0200

    4 fini

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import jwt_utils
 
 app = Flask(__name__)
 
@@ -10,6 +11,15 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def CheckLogin():
+	payload = request.get_json()
+	print(payload['password'])
+	if(payload['password']=="Vive l'ESIEE !"):
+		return jwt_utils.build_token()
+	else:
+		return '', 401
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
