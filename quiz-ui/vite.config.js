7099fed1d8fffb1da4466c495ced6b06e0c4f6bf
Emily Renard <72547692+e-renard@users.noreply.github.com>
2022-05-31 11:14:32 +0200

    workspace

--- quiz-ui/vite.config.js
+++ quiz-ui/vite.config.js
@@ -1,14 +1,25 @@
 import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+export default ({ mode }) => {
+  // eslint-disable-next-line no-undef
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    // eslint-disable-next-line no-undef
+    base: `${process.env.VITE_BASE}`,
+    build: {
+      emptyOutDir: true
     }
-  }
-})
+  });
+}
