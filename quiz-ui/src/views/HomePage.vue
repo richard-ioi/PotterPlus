ae4be0abdd8f0b68f1ffcc922e1ec869fb1e227a
Richard FOUQUOIRE <richard.fouquoire@edu.esiee.fr>
2022-05-31 13:39:20 +0200

    HomePage begin

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+  import QuizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores : []
+    };
+  },
+  async created() {
+    this.registeredScores = QuizApiService.getQuizInfo();
+		console.log("Composant Home page 'created'");
+    console.log("Registered Scores: ", this.registeredScores);
+  }
+};
+</script>
