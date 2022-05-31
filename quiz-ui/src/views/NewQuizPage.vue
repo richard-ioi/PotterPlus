3ea835925142f6351ee49f1816bbc43d9f316541
Emily Renard <72547692+e-renard@users.noreply.github.com>
2022-05-31 18:05:14 +0200

    new quiz page

---
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Quiz page</h1>
+</template>
+
+<script>
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "NewQuizPage",
+  data() {
+    return {
+    };
+  },
+  async created() {
+    console.log("Composant New Quiz Page 'created'");
+  }
+};
+</script>
