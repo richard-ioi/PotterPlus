b24e0e45b0925a77cc212561272ed196bd3ac92c
Emily Renard <72547692+e-renard@users.noreply.github.com>
2022-05-31 17:32:41 +0200

    questions display et manager

---
+++ quiz-ui/src/components/QuestionDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+  <img v-if="question.image" :src="question.image" />
+  <a @click="$emit('answer-selected', 2)">La réponse D</a>
+</template>
+<script>
+  export default {
+    props: {
+      question: {
+        type: Object
+      },
+      emits: ["answer-selected"]
+    }
+  }
+</script>
---
+++ quiz-ui/src/components/QuestionManager.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Question {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}</h1>
+  <QuestionDisplay :question="currentQuestion" @click-on-answer="answerClickedHandler" />
+</template>
+<script>
+import QuestionDisplayVue from './icons/QuestionDisplay.vue'
+export default {
+  components: {
+    QuestionDisplayVue
+  },
+  props: {
+
+  },
+  async created() {
+    console.log("Composant Home page 'created'");
+  },
+  async loadQuestionByPosition() { },
+  async answerClickedHandler() { },
+  async endQuiz() { }
+}
+
+</script>
