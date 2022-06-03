048a294f9fd4c3fcd5a7cba5eadade3eb00ece60
Emily Renard <72547692+e-renard@users.noreply.github.com>
2022-06-03 17:15:37 +0200

    new quiz page updates

--- quiz-ui/src/views/NewQuizPage.vue
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Quiz page</h1>
+  <h1>Start New Quiz</h1>
+  <br>
+  <p>Entrez votre nom :</p>
+  <br>
+  <input v-model="username" type="text" name="name" size="10">
+  <p>{{ username }}</p>
+  <br>
+  <button @click="launchNewQuiz">GO!</button>
 </template>
 
 <script>
@@ -9,10 +16,16 @@ export default {
   name: "NewQuizPage",
   data() {
     return {
-    };
+      username: '',
+    }
   },
   async created() {
     console.log("Composant New Quiz Page 'created'");
+  },
+  methods: {
+    launchNewQuiz() {
+      console.log("Launch new quiz with", this.username);
+    },
   }
 };
 </script>
