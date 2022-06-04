f1bea59fea59432331a5408d172d95b5ad104f9e
Richard FOUQUOIRE <richard.fouquoire@edu.esiee.fr>
2022-06-04 12:43:54 +0200

    Homepage fonctionnel

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,55 +1,45 @@
 <template>
+
   <head>
   </head>
+
   <body>
-    <button id = startBtn type="button" class="btn btn-primary" >Start the quiz</button>
-    <table id =  bestScores class="table table-striped" >
+    <router-link to="/start-new-quiz-page" id=startBtn type="button" class="btn btn-primary">Démarrer le quiz !
+    </router-link>
+    <table id=bestScores class="table table-striped">
       <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Prénom</th>
+          <th scope="col">Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(scoreEntry, index) in this.registeredScores" v-bind:key="scoreEntry.date">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ scoreEntry.playerName }}</td>
+          <td>{{ scoreEntry.score }}</td>
+        </tr>
+      </tbody>
+    </table>
   </body>
-  
+
 </template>
 
 <script>
-  import QuizApiService from "@/services/QuizApiService";
+import QuizApiService from "@/services/QuizApiService";
 
 export default {
   name: "HomePage",
   data() {
     return {
-      registeredScores : []
+      registeredScores: []
     };
   },
   async created() {
-    this.registeredScores = QuizApiService.getQuizInfo();
-		console.log("Composant Home page 'created'");
+    var quizInfoApiResult = await QuizApiService.getQuizInfo();
+    this.registeredScores = quizInfoApiResult.data.scores;
+    console.log("Composant Home page 'created'");
     console.log("Registered Scores: ", this.registeredScores);
   }
 };
